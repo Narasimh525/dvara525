a66ebece333e4909c7eec80609b0bc757c3b7d2c
Narasimha192 <narasimhulu.k@digitalkites.com>
2020-11-18 16:53:26 +0530

    styles added

--- src/components/DisplayCattleInfo.js
+++ src/components/DisplayCattleInfo.js
@@ -10,39 +10,39 @@ class DisplayCattleInfo extends Component{
                 <div style={{margin:"0% 20% 0% 20%"}}>
                     <h3 style={{top:"10%"}}>Cattle Information</h3>
                     <div className="div-parent">
-                        <div className="div-child">Cattle Name</div>
+                        <div className="div-child" id="child1">Cattle Name</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.cattle_name}</div>
+                        <div className="div-child" id="child2">{this.props.details.cattle_name}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Breed</div>
+                        <div className="div-child" id="child1">Breed</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.breed}</div>
+                        <div className="div-child" id="child2">{this.props.details.breed}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Farmer Id</div>
+                        <div className="div-child" id="child1">Farmer Id</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.farmer_ID}</div>
+                        <div className="div-child" id="child2">{this.props.details.farmer_ID}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Farmer Name</div>
+                        <div className="div-child" id="child1">Farmer Name</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.farmer_name}</div>
+                        <div className="div-child" id="child2">{this.props.details.farmer_name}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Weight In Kg</div>
+                        <div className="div-child" id="child1">Weight In Kg</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.weight_in_kg}</div>
+                        <div className="div-child" id="child2">{this.props.details.weight_in_kg}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Stage</div>
+                        <div className="div-child" id="child1">Stage</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.stage}</div>
+                        <div className="div-child" id="child2">{this.props.details.stage}</div>
                     </div>
                     <div className="div-parent">
-                        <div className="div-child">Age In Months</div>
+                        <div className="div-child" id="child1">Age In Months</div>
                         <div className="div-child">:</div>
-                        <div className="div-child">{this.props.details.age_in_months}</div>
+                        <div className="div-child" id="child2">{this.props.details.age_in_months}</div>
                     </div>
                 </div>
             )  
